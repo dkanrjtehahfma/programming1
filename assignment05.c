f613d34a403c92cfff166ea9f60c04a52a1666a0
명식 채 <detective1357@naver.com>
2025-03-31 18:03:32 +0900

    과제#

---
+++ assignment05.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void weight(void) {
+	double weight;
+
+	printf("¸ö¹«°Ô?");
+	scanf ("%21f", &weight);
+
+	printf("dlqfurgks ahaanrpsms %.2fKGÀÔ´Ï´Ù", weight);
+
+}
+
+int main(void)
+{
+	weight();
+	return 0;
+}
